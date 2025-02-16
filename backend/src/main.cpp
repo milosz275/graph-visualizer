8e36b83180cb6411bdc6fe38aeb788e61b7ea3c5
milosz275 <miloszmaculewicz@gmail.com>
2025-02-16 01:15:22 +0100

    Add program runner

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
 
+#define EMSCRIPTEN_GUI 1 // set this to 0 to compile to cli
+
+#if EMSCRIPTEN_GUI == 1
+
+#include <emscripten.h>
+#include <emscripten/html5.h>
+
+#include "render.cpp"
+
+void emscripten_loop()
+{
+    render();
+    // update_graph();
+}
+
+#else
+
+#include "cli.cpp"
+
+void cli_loop()
+{
+    // cli_print_graph();
+    // update_graph();
+}
+
+#endif
+
 using namespace std;
 
 int main()
 {
+#if EMSCRIPTEN_GUI == 1
 
+    EmscriptenWebGLContextAttributes attr;
+    emscripten_webgl_init_context_attributes(&attr);
+    attr.alpha = 0;
+    attr.depth = 0;
+    attr.stencil = 0;
+    attr.antialias = 1;
+    attr.majorVersion = 2;
+
+    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context = emscripten_webgl_create_context("#canvas", &attr);
+    if (context <= 0)
+    {
+        cerr << "Failed to create WebGL context!" << '\n';
+        return 1;
+    }
+
+    emscripten_webgl_make_context_current(context);
+    initGL();
+
+    emscripten_set_main_loop(emscripten_loop, 0, 1);
+
+#else
+
+    cli_loop();
+
+#endif
     return 0;
 }
