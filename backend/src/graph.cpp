90ce1223804775cedbef34b61f67b511f4017705
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 13:19:43 +0100

    Move declarations to header, convert to definition

--- backend/src/graph.cpp
+++ backend/src/graph.cpp
@@ -9,92 +9,55 @@ using namespace std;
 
 static int counter = 0;
 
-class graph_node
+graph_node::graph_node() { id = counter++; }
+
+int graph_node::get_id() const { return id; }
+
+bool graph_node::operator==(const graph_node &other) const { return id == other.id; }
+
+ostream &operator<<(ostream &os, const graph_node &node)
 {
-    int id;
-
-public:
-    graph_node() { id = counter++; }
-    int get_id() const { return id; }
-    bool operator==(const graph_node &other) const { return id == other.id; }
-
-    friend ostream &operator<<(ostream &os, const graph_node &node)
-    {
-        os << "Node(" << node.id << ")";
-        return os;
-    }
-};
-
-struct graph_node_hash
-{
-    size_t operator()(const graph_node &node) const { return hash<int>()(node.get_id()); }
-};
-
-namespace std
-{
-    template <>
-    struct hash<graph_node>
-    {
-        size_t operator()(const graph_node &node) const
-        {
-            return hash<int>()(node.get_id());
-        }
-    };
+    os << "Node(" << node.id << ")";
+    return os;
 }
 
-class directed_graph
+size_t graph_node_hash::operator()(const graph_node &node) const { return hash<int>()(node.get_id()); }
+
+size_t std::hash<graph_node>::operator()(const graph_node &node) const
 {
-    unordered_map<graph_node, unordered_map<graph_node, int>, graph_node_hash> edges;
+    return hash<int>()(node.get_id());
+}
 
-public:
-    unordered_map<graph_node, int> &operator[](const graph_node &node) { return edges[node]; }
-    const unordered_map<graph_node, int> &operator[](const graph_node &node) const { return edges.at(node); }
+unordered_map<graph_node, int> &directed_graph::operator[](const graph_node &node) { return edges[node]; }
 
-    friend ostream &operator<<(ostream &os, const directed_graph &graph)
+const unordered_map<graph_node, int> &directed_graph::operator[](const graph_node &node) const { return edges.at(node); }
+
+ostream &operator<<(ostream &os, const directed_graph &graph)
+{
+    for (const auto &[node, neighbors] : graph.edges)
     {
-        for (const auto &[node, neighbors] : graph.edges)
+        os << node << " -> { ";
+        for (const auto &[neighbor, weight] : neighbors)
         {
-            os << node << " -> { ";
-            for (const auto &[neighbor, weight] : neighbors)
-            {
-                os << neighbor << " = " << weight << "; ";
-            }
-            os << "}\n";
+            os << neighbor << " = " << weight << "; ";
         }
-        return os;
+        os << "}\n";
     }
+    return os;
+}
 
-    void iterate_nodes_once(function<void(const graph_node &)> func) const
+void directed_graph::iterate_nodes_once(function<void(const graph_node &)> func) const
+{
+    for (const auto &[node, _] : edges)
     {
-        for (const auto &[node, _] : edges)
-        {
-            func(node);
-        }
+        func(node);
     }
+}
 
-    int get_node_count() const { return edges.size(); }
+int directed_graph::get_node_count() const { return edges.size(); }
 
-    auto begin() { return edges.begin(); }
-    auto end() { return edges.end(); }
+auto directed_graph::begin() { return edges.begin(); }
+auto directed_graph::end() { return edges.end(); }
 
-    auto begin() const { return edges.begin(); }
-    auto end() const { return edges.end(); }
-};
-
-/*
-directed_graph g;
-graph_node a, b, c;
-
-g[a][b] = 5;
-g[a][c] = 3;
-g[b][c] = 2;
-
-// Print entire graph
-cout << g << endl;
-
-// Iterate through all nodes exactly once
-cout << "Nodes in the graph: ";
-g.iterate_nodes_once([](const graph_node &node)
-                        { cout << node << " "; });
-cout << endl;
-*/
+auto directed_graph::begin() const { return edges.begin(); }
+auto directed_graph::end() const { return edges.end(); }
