f734167cffca1e8c8e500ee81cfdae4fe584b8fb
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 12:56:40 +0100

    Add grid layout, add headers

--- backend/src/render.cpp
+++ backend/src/render.cpp
@@ -1,3 +1,5 @@
+#include "render.h"
+
 #include <iostream>
 
 #include <emscripten.h>
@@ -20,47 +22,33 @@ GLuint VBO, VAO;
 static int canvas_width;
 static int canvas_height;
 
-struct background
+background::background()
 {
-    enum Mode
-    {
-        WHITE,
-        DARK
-    };
+    mode = DARK; // default color mode
 
-    Mode mode;
-    float white_rgb[3];
-    float dark_rgb[3];
+    white_rgb[0] = 0.9f;
+    white_rgb[1] = 0.9f;
+    white_rgb[2] = 0.9f;
 
-    background()
-    {
-        mode = DARK; // default color mode
+    dark_rgb[0] = 0.0f;
+    dark_rgb[1] = 0.1f;
+    dark_rgb[2] = 0.2f;
+}
 
-        white_rgb[0] = 0.9f;
-        white_rgb[1] = 0.9f;
-        white_rgb[2] = 0.9f;
+void background::set_mode(Mode new_mode)
+{
+    mode = new_mode;
+}
 
-        dark_rgb[0] = 0.0f;
-        dark_rgb[1] = 0.1f;
-        dark_rgb[2] = 0.2f;
-    }
-
-    void set_mode(Mode new_mode) { mode = new_mode; }
-    const float *get_current_rgb() const { return (mode == WHITE) ? white_rgb : dark_rgb; }
-};
+const float *background::get_current_rgb() const
+{
+    return (mode == WHITE) ? white_rgb : dark_rgb;
+}
 
 background bg;
 
-class circle
-{
-public:
-    float x, y;
-    float size;
-    float r, g, b;
-};
-
 directed_graph g;
-graph_node a, b, c, d; // ids 0, 1, 2, 3
+graph_node a, b, c, d, e; // ids 0, 1, 2, 3, 4
 
 void init_example_graph()
 {
@@ -70,20 +58,26 @@ void init_example_graph()
     g[b][d] = 2;
     g[c];
     g[d];
+    g[e];
 
     cout << "Generated graph with " << g.get_node_count() << " nodes.\n";
 }
 
-vector<float> generate_render_data()
+vector<float> generate_circle_render_data()
 {
     vector<float> data;
-    float inc = 0.1f;
-    g.iterate_nodes_once([&data, &inc](const graph_node &node)
+
+    // grid layout - no resolution screen resolution independence (scaling)
+    int rows = 2, cols = 2;
+    int i = 0, j = 0;
+    float inc = 0.5f;
+
+    g.iterate_nodes_once([&data, &rows, &cols, &i, &j, &inc](const graph_node &node)
                          {
         circle c;
-        c.x = 0.0f + inc;
-        c.y = 0.0f + inc;
-        c.size = 50.0f;
+        c.x = -0.25f + i * inc;
+        c.y = -0.25f + j * inc;
+        c.size = 40.0f;
         c.r = 1.0f;
         c.g = 0.0f;
         c.b = 1.0f;
@@ -93,7 +87,13 @@ vector<float> generate_render_data()
         data.push_back(c.r);
         data.push_back(c.g);
         data.push_back(c.b);
-        inc += 0.1f; });
+        i++; 
+        if (i == cols)
+        {
+            i = 0;
+            j++;
+        }
+    });
 
     return data;
 }
@@ -106,13 +106,13 @@ void init_gl()
     GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     compile_shader(fragment_shader, fragment_shader_source);
 
-    // Link shaders into a program
+    // link shaders into a program
     shader_program = glCreateProgram();
     glAttachShader(shader_program, vertex_shader);
     glAttachShader(shader_program, fragment_shader);
     glLinkProgram(shader_program);
 
-    // Check for linking errors
+    // check for linking errors
     GLint success;
     glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
     if (!success)
@@ -123,23 +123,23 @@ void init_gl()
              << info_log << '\n';
     }
 
-    // Generate buffer data from graph
-    vector<float> data = generate_render_data();
+    // generate buffer data from graph
+    vector<float> data = generate_circle_render_data();
 
-    // Clean up shaders as they are no longer needed
+    // clean up shaders as they are no longer needed
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
 
-    // Create and bind a Vertex Array Object (VAO)
+    // create and bind a Vertex Array Object (VAO)
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-    // Create and bind a Vertex Buffer Object (VBO)
+    // create and bind a Vertex Buffer Object (VBO)
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), data.data(), GL_STATIC_DRAW);
 
-    // Set up vertex attributes
+    // set up vertex attributes
     GLint pos_attrib = glGetAttribLocation(shader_program, "a_position");
     glEnableVertexAttribArray(pos_attrib);
     glVertexAttribPointer(pos_attrib, 2, GL_FLOAT, GL_FALSE, sizeof(circle), (void *)offsetof(circle, x));
@@ -152,7 +152,7 @@ void init_gl()
     glEnableVertexAttribArray(color_attrib);
     glVertexAttribPointer(color_attrib, 3, GL_FLOAT, GL_FALSE, sizeof(circle), (void *)offsetof(circle, r));
 
-    // Save canvas size
+    // save canvas size
     canvas_width = EM_ASM_INT({ return getCanvasSize().width; }, 0);
     canvas_height = EM_ASM_INT({ return getCanvasSize().height; }, 0);
     cout << "Canvas resolution: " << canvas_width << "x" << canvas_height << '\n';
@@ -160,7 +160,7 @@ void init_gl()
 
 void update_graph()
 {
-    vector<float> data = generate_render_data();
+    vector<float> data = generate_circle_render_data();
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), data.data(), GL_STATIC_DRAW);
