bc0e1d1a579c16ac72741a70edec8ea9b0bcd590
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 18:49:34 +0100

    Add explicit node declaration

--- backend/src/cli.cpp
+++ backend/src/cli.cpp
@@ -11,12 +11,17 @@ graph_node a, b, c, d; // ids 0, 1, 2, 3
 
 void init_example_graph()
 {
+    // add nodes
+    g[a];
+    g[b];
+    g[c];
+    g[d];
+
+    // define edges
     g[a][b] = 5;
     g[a][c] = 3;
     g[b][c] = 2;
     g[b][d] = 2;
-    g[c];
-    g[d];
 
     cout << "Generated graph with " << g.get_node_count() << " nodes.\n";
 }
