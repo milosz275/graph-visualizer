ff9902766bf71a1827a893fcd3c3628c8ded50a3
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 13:19:11 +0100

    Add cli.h

---
+++ backend/include/cli.h
@@ -0,0 +1,19 @@
+#ifndef CLI_H
+#define CLI_H
+
+/**
+ * @brief Initializes an example graph with predefined nodes and edges.
+ */
+void init_example_graph();
+
+/**
+ * @brief Updates the graph data.
+ */
+void update_graph();
+
+/**
+ * @brief Prints the graph to the console.
+ */
+void cli_print_graph();
+
+#endif // CLI_H
