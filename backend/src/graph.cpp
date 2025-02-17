dfa2dac5fcc0dc6bacae90a419a41abcf1e0fae9
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 18:51:56 +0100

    Update node constructor, update graph to stream handling, add graph sorted node iteration, add edge count getter

--- backend/src/graph.cpp
+++ backend/src/graph.cpp
@@ -9,17 +9,18 @@ using namespace std;
 
 static int counter = 0;
 
-graph_node::graph_node() { id = counter++; }
+graph_node::graph_node()
+{
+    id = counter++;
+    x = 0.0f;
+    y = 0.0f;
+}
 
 int graph_node::get_id() const { return id; }
 
-float graph_node::get_x() const { return x; }
+bool graph_node::get_selected() const { return selected; }
 
-float graph_node::get_y() const { return y; }
-
-void graph_node::set_x(float x) { this->x = x; }
-
-void graph_node::set_y(float y) { this->y = y; }
+void graph_node::set_selected(bool selected) { this->selected = selected; }
 
 bool graph_node::operator==(const graph_node &other) const { return id == other.id; }
 
@@ -42,6 +43,7 @@ const unordered_map<graph_node, int> &directed_graph::operator[](const graph_nod
 
 ostream &operator<<(ostream &os, const directed_graph &graph)
 {
+    os << "Graph nodes:\n";
     for (const auto &[node, neighbors] : graph.edges)
     {
         os << node << " -> { ";
@@ -51,14 +53,37 @@ ostream &operator<<(ostream &os, const directed_graph &graph)
         }
         os << "}\n";
     }
+    graph.iterate_nodes([&os](graph_node &node)
+                        { os << node << "is selected: " << node.get_selected() << '\n'; });
+
+    os << "Graph edges\n";
+    graph.iterate_edges([&os](const graph_node &start, const graph_node &end)
+                        { os << start << " -> " << end << '\n'; });
     return os;
 }
 
-void directed_graph::iterate_nodes(function<void(graph_node &)> func)
+void directed_graph::iterate_nodes(function<void(graph_node &)> func) const
 {
     for (auto &pair : edges)
     {
-        func(const_cast<graph_node&>(pair.first));
+        func(const_cast<graph_node &>(pair.first));
+    }
+}
+
+void directed_graph::iterate_nodes_by_id(function<void(graph_node &)> func) const
+{
+    vector<graph_node> nodes;
+    for (const auto &pair : edges)
+    {
+        nodes.push_back(pair.first);
+    }
+
+    sort(nodes.begin(), nodes.end(), [](const graph_node &a, const graph_node &b)
+         { return a.get_id() < b.get_id(); });
+
+    for (auto &node : nodes)
+    {
+        func(const_cast<graph_node &>(node));
     }
 }
 
@@ -74,6 +99,13 @@ void directed_graph::iterate_edges(function<void(const graph_node &, const graph
 }
 
 int directed_graph::get_node_count() const { return edges.size(); }
+int directed_graph::get_edge_count() const
+{
+    int count = 0;
+    iterate_edges([&count](const graph_node &, const graph_node &)
+                  { count++; });
+    return count;
+}
 
 auto directed_graph::begin() { return edges.begin(); }
 auto directed_graph::end() { return edges.end(); }
