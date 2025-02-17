aac43eadcac3fd723f5070e84a94838580f881ab
milosz275 <miloszmaculewicz@gmail.com>
2025-02-17 18:54:16 +0100

    Add edge shaders

--- backend/src/shader.cpp
+++ backend/src/shader.cpp
@@ -26,26 +26,49 @@ void compile_shader(GLuint shader, const char *source)
     }
 }
 
-const char *vertex_shader_source = R"(
+const char *circle_vertex_shader_source = R"(
     attribute vec2 a_position;
     attribute float a_size;
     attribute vec3 a_color;
     varying vec3 v_color;
-    void main() {
+    void main()
+    {
         gl_Position = vec4(a_position, 0.0, 1.0);
         gl_PointSize = a_size;
         v_color = a_color;
     }
 )";
 
-const char *fragment_shader_source = R"(
+const char *circle_fragment_shader_source = R"(
     precision mediump float;
     varying vec3 v_color;
-    void main() {
+    void main()
+    {
         vec2 coord = gl_PointCoord - vec2(0.5);
-        if (dot(coord, coord) > 0.25) {
+        if (dot(coord, coord) > 0.25)
+        {
             discard;
         }
+        gl_FragColor = vec4(v_color, 1.0);
+    }
+)";
+
+const char *edge_vertex_shader_source = R"(
+    attribute vec2 a_position;
+    attribute vec3 a_color;
+    varying vec3 v_color;
+    void main()
+    {
+        gl_Position = vec4(a_position, 0.0, 1.0);
+        v_color = a_color;
+    }
+)";
+
+const char *edge_fragment_shader_source = R"(
+    precision mediump float;
+    varying vec3 v_color;
+    void main()
+    {
         gl_FragColor = vec4(v_color, 1.0);
     }
 )";
