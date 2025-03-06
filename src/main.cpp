74696e9f9c0ffb70ba0e4ec36cb6ab55af51046d
milosz275 <miloszmaculewicz@gmail.com>
2025-03-06 10:29:44 +0100

    Add sierpinski triangle demo

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 #include <GLES3/gl3.h>
+#include <glm/glm.hpp>
 
 #include "app.h"
 #include "renderer.h"
@@ -11,33 +12,37 @@
 
 using namespace std;
 
+void draw_sierpinski_triangle(glm::vec2 p1, glm::vec2 p2, glm::vec2 p3, int depth)
+{
+    if (depth == 0)
+        web_ui::renderer::draw_triangle(p1, p2, p3, glm::vec3(1.0f, 1.0f, 1.0f));
+    else
+    {
+        glm::vec2 mid1 = (p1 + p2) / 2.0f;
+        glm::vec2 mid2 = (p2 + p3) / 2.0f;
+        glm::vec2 mid3 = (p3 + p1) / 2.0f;
+
+        draw_sierpinski_triangle(p1, mid1, mid3, depth - 1);
+        draw_sierpinski_triangle(mid1, p2, mid2, depth - 1);
+        draw_sierpinski_triangle(mid3, mid2, p3, depth - 1);
+    }
+}
+
 void render_loop()
 {
-    // drawing
     web_ui::background::draw_background();
-    web_ui::renderer::draw_circle({0.75f, 0.75f}, 0.200f, {1, 0.80f, 0}); // sun
-    web_ui::renderer::draw_circle({0.75f, 0.75f}, 0.175f, {1, 0.90f, 0}); // sun
-    web_ui::renderer::draw_circle({0.75f, 0.75f}, 0.150f, {1, 1.00f, 0}); // sun
-    web_ui::renderer::draw_rectangle({-0.5f, -0.5f}, {0.5, 0.25f}, {0.8f, 0.5f, 0.2f}); // house base
-    web_ui::renderer::draw_triangle({-0.75f, 0.25f}, {0.75f, 0.25f}, {0.0f, 0.75f}, {0.6f, 0.3f, 0.1f}); // roof
-    web_ui::renderer::draw_rectangle({-0.1f, -0.5f}, {0.1f, 0.0f}, {0.4f, 0.2f, 0.1f}); // door
-    web_ui::renderer::draw_rectangle({-0.4f, -0.2f}, {-0.2f, 0.0f}, {0.7f, 0.9f, 1.0f}); // left window
-    web_ui::renderer::draw_rectangle({0.2f, -0.2f}, {0.4f, 0.0f}, {0.7f, 0.9f, 1.0f}); // right window
-    web_ui::renderer::draw_line({-0.4f, -0.1f}, {-0.2f, -0.1f}, {0, 0, 0}); // left window horizontal window pane
-    web_ui::renderer::draw_line({-0.3f, -0.2f}, {-0.3f, 0.0f}, {0, 0, 0}); // left window vertical window pane
-    web_ui::renderer::draw_line({0.2f, -0.1f}, {0.4f, -0.1f}, {0, 0, 0}); // right window horizontal window pane
-    web_ui::renderer::draw_line({0.3f, -0.2f}, {0.3f, 0.0f}, {0, 0, 0}); // right window vertical window pane
-
-    // text
     web_ui::text::clear_text_canvas();
     web_ui::text::draw_text_absolute();
-    web_ui::text::draw_text({-0.03f, -0.75f}, "House", "20px serif", "brown");
+
+    glm::vec2 p1 = glm::vec2(-0.5f, -0.5f);
+    glm::vec2 p2 = glm::vec2(0.5f, -0.5f);
+    glm::vec2 p3 = glm::vec2(0.0f, 0.5f);
+
+    draw_sierpinski_triangle(p1, p2, p3, 5);
 }
 
 int main()
 {
-    web_ui::text::setup_canvas();
-
     EmscriptenWebGLContextAttributes attr;
     emscripten_webgl_init_context_attributes(&attr);
     attr.alpha = 0;
@@ -45,7 +50,7 @@ int main()
     attr.stencil = 1;
     attr.antialias = 1;
     attr.majorVersion = 2;
-
+    
     EMSCRIPTEN_WEBGL_CONTEXT_HANDLE context = emscripten_webgl_create_context("#webglCanvas", &attr);
     if (context <= 0)
     {
@@ -55,6 +60,7 @@ int main()
 
     emscripten_webgl_make_context_current(context);
 
+    web_ui::text::setup_canvas();
     web_ui::renderer::init();
     web_ui::app::run(render_loop);
 
