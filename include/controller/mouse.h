22b1ee6dbfce09d6f6a20c32314d64aaa2190b16
milosz275 <miloszmaculewicz@gmail.com>
2025-03-10 00:34:25 +0100

    Update header

--- include/controller/mouse.h
+++ include/controller/mouse.h
@@ -3,6 +3,8 @@
 #include <emscripten.h>
 #include <emscripten/html5.h>
 
+#include <glm/vec2.hpp>
+
 namespace mvc
 {
     class mouse
@@ -11,6 +13,7 @@ namespace mvc
         static void connect_mouse_callbacks();
 
     private:
+
         static EM_BOOL mouse_click_callback(int event_type, const EmscriptenMouseEvent* e, void* user_data);
         static EM_BOOL mouse_down_callback(int event_type, const EmscriptenMouseEvent* e, void* user_data);
         static EM_BOOL mouse_up_callback(int event_type, const EmscriptenMouseEvent* e, void* user_data);
