bc3c161875349a3579bf9426feef826ff9b5cded
milosz275 <miloszmaculewicz@gmail.com>
2025-03-10 00:41:13 +0100

    Adjust background color

--- src/web/background.cpp
+++ src/web/background.cpp
@@ -6,7 +6,7 @@ namespace web_ui
 {
     void background::draw_background()
     {
-        glClearColor(0.6f, 0.6f, 0.6f, 1.0f);
+        glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
     }
 }
