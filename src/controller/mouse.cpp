8cb3f1478e84932b066c9bf84912859226393748
milosz275 <miloszmaculewicz@gmail.com>
2025-03-10 23:06:18 +0100

    Add mouse cursor setters

--- src/controller/mouse.cpp
+++ src/controller/mouse.cpp
@@ -7,12 +7,24 @@
 
 namespace mvc
 {
+    bool mouse::hover = false;
+
     void mouse::connect_mouse_callbacks()
     {
         emscripten_set_click_callback("#webglCanvas", nullptr, EM_TRUE, mouse_click_callback);
         emscripten_set_mousedown_callback("#webglCanvas", nullptr, EM_TRUE, mouse_down_callback);
         emscripten_set_mouseup_callback("#webglCanvas", nullptr, EM_TRUE, mouse_up_callback);
         emscripten_set_mousemove_callback("#webglCanvas", nullptr, EM_TRUE, mouse_move_callback);
+    }
+
+    void mouse::set_mouse_cursor()
+    {
+        EM_ASM_INT({ return setMouseCursor(); }, 0);
+    }
+
+    void mouse::set_mouse_pointer()
+    {
+        EM_ASM_INT({ return setMousePointer(); }, 0);
     }
 
     EM_BOOL mouse::mouse_click_callback(int event_type, const EmscriptenMouseEvent* e, void* user_data)
@@ -34,6 +46,7 @@ namespace mvc
 
     EM_BOOL mouse::mouse_move_callback(int event_type, const EmscriptenMouseEvent* e, void* user_data)
     {
+        app::graph_app::handle_mouse_move(view::get_coordinates({e->clientX, e->clientY}));
         return EM_TRUE;
     }
 }
