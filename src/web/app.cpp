09565a58b47038eb5c9c00b23527f8b5ec700008
milosz275 <miloszmaculewicz@gmail.com>
2025-03-10 23:17:11 +0100

    Update app setup

--- src/web/app.cpp
+++ src/web/app.cpp
@@ -31,7 +31,6 @@ namespace app
 
     void graph_app::set_state(std::unique_ptr<mvc::ui_state> new_state)
     {
-        std::cout << "Going to new state: " << new_state->get_label() << '\n';
         current_state = std::move(new_state);
     }
 
@@ -50,26 +49,20 @@ namespace app
 
     void graph_app::main_loop()
     {
-        render_cycle();
-        simulation_cycle();
-    }
-
-    void graph_app::render_cycle()
-    {
         web_ui::background::draw_background();
         web_ui::text::clear_text_canvas();
-        web_ui::text::draw_text_absolute();
+        web_ui::text::draw_text_absolute(); // logo
 
         current_state->render();
-    }
-
-    void graph_app::simulation_cycle()
-    {
-        // graph state and algorithm state have the graph, that needs to be called with .apply_physics()
     }
 
     void graph_app::handle_mouse_click(glm::vec2 mouse, bool down)
     {
         current_state->handle_click(mouse, down);
+    }
+
+    void graph_app::handle_mouse_move(glm::vec2 mouse)
+    {
+        current_state->handle_hover(mouse);
     }
 }
