a3689b63925cb8fe53f089364a53023786bce1a9
milosz275 <miloszmaculewicz@gmail.com>
2025-03-22 17:01:22 +0100

    Add OOP

--- include/model/graph_node.h
+++ include/model/graph_node.h
@@ -3,22 +3,69 @@
 #include <vector>
 #include <cmath>
 
+#include <glm/vec2.hpp>
+
 namespace mvc
 {
-    struct graph_node
+    class graph;
+    class graph_physics;
+
+    class graph_node
     {
+        static int node_counter;
         int id;
-    
-        // logical data (for algorithms)
+        
+        glm::vec2 position;
+        glm::vec2 velocity;
+        glm::vec2 force_accumulator;
+        
         std::vector<int> neighbors;
-        bool highlighted = false;
-        bool visited = false;
-        float distance = INFINITY;
-        int parent = -1;
-    
-        // physical data (for rendering)
-        float x, y;            // position
-        float vx = 0, vy = 0;  // velocity
-        float fx = 0, fy = 0;  // force accumulators
+        bool visited;
+        bool highlighted;
+        int parent;
+
+    public:
+        /**
+         * 
+         */
+        graph_node();
+
+        /**
+         * 
+         */
+        graph_node(float x, float y);
+
+        /**
+         * 
+         */
+        graph_node(glm::vec2 position);
+
+        /**
+         * 
+         */
+        bool get_visited();
+        
+        /**
+         * 
+         */
+        bool set_visited(bool visited);
+        
+        /**
+         * 
+         */
+        bool set_highlighted(bool highlighted);
+
+        /**
+         * 
+         */
+        int set_parent(int parent);
+
+        /**
+         * 
+         */
+        std::vector<int>& get_neighbors();
+
+        friend class graph;
+        friend class graph_physics;
     };
 }
